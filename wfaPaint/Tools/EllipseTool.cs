0c6a1f2d2e30069027cdef06b2fa49c6665c6290
zoomero8 <127130537+zoomero8@users.noreply.github.com>
2025-06-09 14:21:18 +0300

    Add files via upload

---
+++ wfaPaint/Tools/EllipseTool.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using wfaPaint;
+
+namespace wfaPaint.Tools
+{
+    public class EllipseTool : ShapeToolBase
+    {
+        public override string Name => "Ellipse";
+
+        protected override void DrawShape(Graphics g, Point currentPoint, Pen pen)
+        {
+            int x = Math.Min(startPoint.X, currentPoint.X);
+            int y = Math.Min(startPoint.Y, currentPoint.Y);
+            int width = Math.Abs(startPoint.X - currentPoint.X);
+            int height = Math.Abs(startPoint.Y - currentPoint.Y);
+
+            RectangleF ellipseRect = new RectangleF(x, y, width, height);
+            g.DrawEllipse(pen, ellipseRect);
+        }
+    }
+}
---
+++ wfaPaint/Tools/ITool.cs
@@ -0,0 +1,24 @@
+﻿// --- ИЗМЕНИТЬ ФАЙЛ ITool.cs ---
+
+using System.Drawing;
+using System.Windows.Forms;
+using wfaPaint;
+
+namespace wfaPaint.Tools
+{
+    public interface ITool
+    {
+        string Name { get; }
+        Cursor ToolCursor { get; }
+
+        void OnMouseDown(MouseEventArgs e, DrawingCanvas canvas, ToolSettings settings);
+        void OnMouseMove(MouseEventArgs e, DrawingCanvas canvas, ToolSettings settings);
+        void OnMouseUp(MouseEventArgs e, DrawingCanvas canvas, ToolSettings settings);
+    }
+
+    // (ИЗМЕНЕНИЕ) Новый интерфейс для интерактивных инструментов
+    public interface IInteractiveTool
+    {
+        void OnMouseWheel(MouseEventArgs e);
+    }
+}
